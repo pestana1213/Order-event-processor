3f1748ec01808c364c972779b64ba0c1306b9889
Joao <pestanapt@gmail.com>
2025-03-06 21:08:08 +0000

    first commit

---
+++ src/main/java/com/example/order_events_processor/OrderEventsProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.example.order_events_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderEventsProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderEventsProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/order_events_processor/OrderEventsProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.order_events_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderEventsProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
